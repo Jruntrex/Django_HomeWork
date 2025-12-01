92632da009d8b72c54a9026eda8c150b04d03cba
Serhii Hodis <serhdg12@gmail.com>
2025-12-01 10:35:55 +0200

    Add PW 2.3: Guess Game App

--- pw_2.3/myproj/urls.py
+++ pw_2.3/myproj/urls.py
@@ -5,9 +5,7 @@ from guessgame_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Головна сторінка гри. name='game_url' потрібен для функції redirect()
     path('guess/', views.game_page, name='game_url'),
     
-    # Додатковий шлях для скидання гри
     path('reset/', views.reset_game),
 ]
